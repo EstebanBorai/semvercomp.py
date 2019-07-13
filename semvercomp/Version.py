823dd49d5937dab9b78c08467228d2af109cd8e2
Esteban Borai <estebanborai@outlook.com>
2019-07-13 16:47:50 -0300

    Add support to validate tags

--- semvercomp/Version.py
+++ semvercomp/Version.py
@@ -1,20 +1,46 @@
+import re as regexp
+from semvercomp.validators import validate_version
+
 class Version():
 	"""
 		Version represents a semantic version number
 	"""
 
-	def __init__(self, major=0, minor=0, patch=0):
+	def __init__(self, major=0, minor=0, patch=0, pre_release="", build="", has_v=False):
+		self.has_v = has_v
 		self.major = major
 		self.minor = minor
 		self.patch = patch
+		self.pre_release = pre_release
+		self.build = build
 		self.version_number = self.__str__()
 
 	def __str__(self):
-		return f'{self.major}.{self.minor}.{self.patch}'
+		base = ''
+
+		if self.has_v:
+			base = 'v'
+
+		base = f'{base}{self.major}.{self.minor}.{self.patch}'
+
+		if self.pre_release != "":
+			base = f'{base}-{self.pre_release}'
+
+		if self.build != "":
+			base = f'{base}+{self.build}'
+
+		return base
 
 	def parse_version_number(self, version_string):
-		[major, minor, patch] = version_string.split('.')
-		self.major = int(major)
-		self.minor = int(minor)
-		self.patch = int(patch)
-		self.version_number = self.__str__()
+		(parts, is_ok) = validate_version(version_string)
+
+		if is_ok:
+			self.has_v = parts['has_v']
+			self.major = int(parts['major'])
+			self.minor = int(parts['minor'])
+			self.patch = int(parts['patch'])
+			self.pre_release = parts['pre_release']
+			self.build = parts['build']
+			self.version_number = version_string
+		else:
+			raise Exception(f'Version string {version_string} is not a valid version tag.')
--- tests/test_Version.py
+++ tests/test_Version.py
@@ -20,10 +20,28 @@ def test_Version_init_patch():
 	ver = Version(None, None, patch)
 	assert ver.patch == patch
 
-def test_Version__str__():
-	v_string = "1.21.11"
+def test_Version_init_pre_release():
+	pre_release = 'beta'
 
-	ver = Version(1, 21, 11)
+	ver = Version(None, None, None, pre_release)
+	assert ver.pre_release == pre_release
+
+def test_Version_init_build():
+	build = '123322'
+
+	ver = Version(None, None, None, None, build)
+	assert ver.build == build
+
+def test_Version_init_has_v():
+	has_v = True
+
+	ver = Version(None, None, None, None, None, True)
+	assert ver.has_v == has_v
+
+def test_Version__str__():
+	v_string = "v1.21.11-beta+123123"
+
+	ver = Version(1, 21, 11, 'beta', '123123', True)
 	assert v_string == str(ver)
 
 def test_Version_parse_version_number():
@@ -35,3 +53,12 @@ def test_Version_parse_version_number():
 	assert ver.major == 19
 	assert ver.minor == 22
 	assert ver.patch == 133
+
+def test_Version_raise_invalid_tag_exception():
+	v_string = "v01.22.43-beta"
+	ver = Version()
+
+	with pytest.raises(Exception) as exc:
+		assert ver.parse_version_number(v_string)
+
+	assert str(exc.value) == f'Version string {v_string} is not a valid version tag.'	
