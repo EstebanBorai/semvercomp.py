03787fc67592b0a3ad6355fab22206547306579c
Esteban Borai <estebanborai@outlook.com>
2019-07-14 14:43:47 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="semvercomp",
-  version="0.1.0",
+  version="0.1.1",
   author="Esteban Borai",
   author_email="estebanborai@outlook.com",
   description="Semantic Version Comparison for Python",
